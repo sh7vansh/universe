8e95c722f11a701655ef5664f7f32eeaefe842e8
Shivansh Singh <2310shivansh@gmail.com>
2026-09-14 05:11:52 +0530

    Remove test files

--- math_project/test2.lean
+++
@@ -1,5 +0,0 @@
-import Mathlib.Order.Cover
-import Mathlib.Order.CompleteLattice.Basic
-import Mathlib.Order.Atoms
-
-#check IsAtom
--- math_project/test3.lean
+++
@@ -1,9 +0,0 @@
-import Mathlib.Order.Cover
-import Mathlib.Order.Atoms
-import Mathlib.Order.CompleteLattice.Basic
-
-variable {L : Type*} [CompleteLattice L]
-
-def MacLaneSteinitz : Prop :=
-  ∀ a b x : L, IsAtom a → IsAtom b → a ≤ x ⊔ b → ¬(a ≤ x) → b ≤ x ⊔ a
-
